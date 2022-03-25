53e7ec2b6f30bbb719f0ac25e8883ed679ebf53d
Becky Everson <reverson94@gmail.com>
2022-03-25 09:48:14 -0400

    Fix General category bug and add keywords to category objects

--- scripts/analyze_chapters.py
+++ scripts/analyze_chapters.py
@@ -1,12 +1,11 @@
 import os
-import pprint
 import pandas as pd
 import numpy as np
 import re
 from collections import namedtuple
 
 # CHANGE THIS FILE PATH TO YOUR FOLDER WITH THE GRAMMAR SPREADSHEETS
-loc = (r"C:\Users\rever\OneDrive\Desktop\grammar_project\Spreadsheets")
+loc = (r"C:\..\Spreadsheets")
 
 col_labels = ['phenomenon id', 'description', 'category', 'page number', 'chapter',
     'comment', 'explanation for variation', 'keyword', 'cross-ref/chapter', 'page cross-ref']
@@ -17,7 +16,7 @@ ColumnLabels = namedtuple('ColumnLabels', (
 
 categories = ['phonetic', 'syllabic', 'grammatical word form', 'lexical word form',
     'word set alternation', 'inflectional affixes', 'derivational affixes', 'gender',
-    'syntactic', 'historical', 'orthographic']
+    'syntactic', 'historical', 'orthographic', 'general']
 
 chapters = ['introduction', 'phonology', 'word classes', 'morphology', 'np structure',
     'vp structure', 'clause structure', 'complex clauses', 'lexicon appendix',
@@ -40,50 +39,75 @@ def setup_cat_dict():
     # Initialize the category dictionaries
     file_cat_dict = {cat: {'count': 0} for cat in categories}
     file_cat_dict['uncategorized'] = {'count': 0}
-    file_cat_dict['general'] = {'count': 0}
     for category in file_cat_dict:
         file_cat_dict[category]['chapters'] = dict.fromkeys(chapters, 0)
         file_cat_dict[category]['explanations for variation'] = dict.fromkeys(explanations, 0)
+        file_cat_dict[category]['keywords'] = {}
     return file_cat_dict
 
-def analyze_categories(values, chap_values, exp_values, cat_dict):
+def analyze_categories(values, chap_values, exp_values, keyword_values, cat_dict):
+    # Initialize spreadsheet JSON object
     file_cat_dict = setup_cat_dict()
+    # Use category column to loop through all values
+    # Note: main column used for all other data comparison is "Category"
     for index, category in enumerate(values):
+        # General case (no specific category for variation)
         if category == 0:
-            lower_cat = 'general'
-        else:
-            cats = re.split(',\s*', category)
-            for cat in cats:
-                row_num = index + 2
-                chapter = chap_values[index]
-                explanation = exp_values[index]
-                lower_chap = chapter.lower()
-                if not isinstance(explanation, str) and np.isnan(explanation):
-                    explanation = lower_exp = 'no explanation'
-                else:
-                    lower_exp = explanation.lower()
-                if cat == 0:
-                    lower_cat = 'general'
-                else:
-                    lower_cat = cat.lower().rstrip()
-                    if lower_cat not in categories:
-                        lower_cat = 'uncategorized'
-                if lower_chap not in chapters:
-                    lower_chap = 'n/a'
+            category = 'general'
+        # Category column can have multiple values comma-separated
+        cats = re.split(',\s*', category)
+        for cat in cats:
+            # Excel spreadsheet row offset
+            row_num = index + 2
+            # Get the chapter column values at this row
+            chapter = chap_values[index]
+            # Get the explanation column values at this row
+            explanation = exp_values[index]
+            lower_chap = chapter.lower()
+            # If the Excel cell is empty, it will register as NaN, which behaves weirdly
+            if not isinstance(explanation, str) and np.isnan(explanation):
+                explanation = lower_exp = 'no explanation'
+            else:
+                lower_exp = explanation.lower()
+            lower_cat = cat.lower().rstrip()
+            # Only assess categories that are specified in Project Steps doc
+            if lower_cat not in categories:
+                lower_cat = 'uncategorized'
+            # Same with chapters as with categories
+            if lower_chap not in chapters:
+                lower_chap = 'n/a'
 
-                cat_dict['totals'][lower_cat]['count'] += 1
-                cat_dict['totals'][lower_cat]['chapters'][lower_chap] += 1
+            # Update count values
+            cat_dict['totals'][lower_cat]['count'] += 1
+            cat_dict['totals'][lower_cat]['chapters'][lower_chap] += 1
+            file_cat_dict[lower_cat]['count'] += 1
+            file_cat_dict[lower_cat]['chapters'][lower_chap] += 1
 
+            # Like category column, explanation column can have multiple comma-separated values
+            if lower_exp != 'no explanation':
                 exps = re.split(',\s*', lower_exp)
-                if lower_exp != 'no explanation':
-                    exps = re.split(',\s*', lower_exp)
-                    for exp in exps:
-                        exp = exp.strip()
-                        cat_dict['totals'][lower_cat]['explanations for variation'][exp] += 1
-                        file_cat_dict[lower_cat]['explanations for variation'][exp] += 1
+                for exp in exps:
+                    exp = exp.strip()
+                    cat_dict['totals'][lower_cat]['explanations for variation'][exp] += 1
+                    file_cat_dict[lower_cat]['explanations for variation'][exp] += 1
 
-                       file_cat_dict[lower_cat]['count'] += 1
-                file_cat_dict[lower_cat]['chapters'][lower_chap] += 1
+            words = keyword_values[index]
+            # Same with keywords as with explanations
+            if not isinstance(words, str) and np.isnan(words):
+                words = []
+            else:
+                keywords = re.split(',\s*', words)
+                for keyword in keywords:
+                    # Keywords dict is not initialized with keys since there are so many, so add them
+                    # as they are found
+                    if keyword in file_cat_dict[lower_cat]['keywords']:
+                        file_cat_dict[lower_cat]['keywords'][keyword] += 1
+                    else:
+                        file_cat_dict[lower_cat]['keywords'][keyword] = 1
+                    if keyword in cat_dict['totals'][lower_cat]['keywords']:
+                        cat_dict['totals'][lower_cat]['keywords'][keyword] += 1
+                    else:
+                        cat_dict['totals'][lower_cat]['keywords'][keyword] = 1
 
     return dict(sorted(file_cat_dict.items(), key=lambda item: item[1]['count'], reverse=True))
 
@@ -128,13 +152,14 @@ if __name__ == "__main__":
         desc_values = df.get('description')
         chap_values = df.get('chapter')
         exp_values = df.get('explanation for variation')
+        keyword_values = df.get('keyword')
         if cat_values.any() and desc_values.any():
-            cat_dict['spreadsheets'][filename] = analyze_categories(cat_values, chap_values, exp_values, cat_dict)
+            cat_dict['spreadsheets'][filename] = analyze_categories(
+                cat_values, chap_values, exp_values, keyword_values, cat_dict)
         else:
             print("CATEGORY COLUMN DOES NOT EXIST")
 
         print(f"\n..Checking for keywords in {filename}")
-        keyword_values = df.get('keyword')
         if keyword_values.any():
             keyword_dict['spreadsheets'][filename] = get_keywords(keyword_values, keyword_dict)
         else:
